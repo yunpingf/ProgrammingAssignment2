20772aa0bf522230c21a836b2e53de313fb90810
Feng Yunping <fengyunping1992@gmail.com>
2015-03-22 23:10:01 +0800

    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    r <- NULL
+    set <- function(y) {
+        x <<- y
+        r <<- NULL
+    }
+    get <- function() x
+    setRev <- function(rev) r <<- rev
+    getRev <- function() r
+    list (set = set, get = get,
+          setRev = setRev,
+          getRev = getRev)
 }
 
 
@@ -12,4 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    r <- x$getRev()
+    if (!is.null(r)) {
+        message("getting inverse of the matrix")
+        return (r)
+    }
+    m <- x$get()
+    r <- solve(m)
+    x$setRev(r)
+    r
 }
